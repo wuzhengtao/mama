acc14d3a167ce57dfa73f9d4f374b832015216ab
wuzhengtao <8068907+wuzhengtao@user.noreply.gitee.com>
2022-05-16 20:55:12 +0800

    import test

---
+++ js/import.js
@@ -0,0 +1,13 @@
+function readFile(obj) {
+    var file = obj.files[0];
+    var reader = new FileReader();
+
+    reader.readAsBinaryString(file);
+    reader.onload = function (e) {
+        var data = e.target.result;
+        var wb = XLSX.read(data, {type: "binary"});
+
+        //此处对excel数据进行处理
+        console.log(wb);
+    };
+}
